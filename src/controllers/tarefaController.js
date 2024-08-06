29018e8f6166285bbacf2677d3793464afbf282b
elisa marques <elisamarques466@gmail.com>
2024-08-05 22:12:29 -0300

    gerenciador de tarefa

---
+++ src/controllers/tarefaController.js
@@ -0,0 +1,18 @@
+function exibirTarefas(request, response) {
+    response.render("index");
+}
+
+function exibirNovaTarefa(request, response) {
+    response.render("adicionarTarefa");
+}
+
+function adicionarTarefa(request, response) {
+    console.log('Chegou na adicionarTarefa');
+    response.redirect("/");
+}
+
+module.exports = {
+    exibirTarefas,
+    exibirNovaTarefa,
+    adicionarTarefa
+}
